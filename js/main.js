beae0352510c06a1009dd305742900031c67e9ed
shaker <shaker.hossain87@gmail.com>
2021-08-25 18:11:06 +0600

    biopit code done

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const countDownClock = (number = 100, format = 'seconds') => {
+  
+    const d = document;
+ 
+    const hoursElement = d.querySelector('.hours');
+    const minutesElement = d.querySelector('.minutes');
+    const secondsElement = d.querySelector('.seconds');
+    let countdown;
+    convertFormat(format);
+    
+    
+    function convertFormat(format) {
+      switch(format) {
+        case 'seconds':
+          return timer(number);
+        case 'minutes':
+          return timer(number * 60);
+          case 'hours':
+          return timer(number * 60 * 60);
+                 
+      }
+    }
+  
+    function timer(seconds) {
+      const now = Date.now();
+      const then = now + seconds * 1000;
+  
+      countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+  
+        if(secondsLeft <= 0) {
+          clearInterval(countdown);
+          return;
+        };
+  
+        displayTimeLeft(secondsLeft);
+  
+      },1000);
+    }
+  
+    function displayTimeLeft(seconds) {
+     
+        hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
+        //console.log(`second`, Math.floor(seconds / 86400))
+      minutesElement.textContent = Math.floor((seconds % 86400) % 3600 / 60);
+      secondsElement.textContent = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
+    }
+  }
+  
+  
+  /*
+    start countdown
+    enter number and format
+    days, hours, minutes or seconds
+  */
+  countDownClock(20, 'hours');
